5eaa9f45e3ea56ffac6eef2b51c10c185302d5ae
Phan Tan Dung <phantandung92@gmail.com>
2014-05-01 16:13:09 +0700

    Cập nhật mô tả các file

--- modules/gallery-tool/blocks/global.marquee.php
+++ modules/gallery-tool/blocks/global.marquee.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * @Project NUKEVIET 3.0
- * @Author VINADES.,JSC (contact@vinades.vn)
- * @Copyright (C) 2010 VINADES., JSC. All rights reserved
- * @Createdate 3/9/2010 23:25
+ * @Project NUKEVIET GALLERY TOOL 3.x
+ * @Author PHAN TAN DUNG (phantandung92@gmail.com)
+ * @Copyright (C) 2014 PHAN TAN DUNG. All rights reserved
+ * @Createdate May 01, 2014, 04:09:03 PM
  */
 
 if( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
--- modules/gallery-tool/language/vi.php
+++ modules/gallery-tool/language/vi.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * @Project NUKEVIET BLOG 3.x
+ * @Project NUKEVIET GALLERY TOOL 3.x
  * @Author PHAN TAN DUNG (phantandung92@gmail.com)
- * @Copyright (C) 2013 PHAN TAN DUNG. All rights reserved
+ * @Copyright (C) 2014 PHAN TAN DUNG. All rights reserved
  * @Language Tiếng Việt
- * @Createdate Dec 11, 2013, 09:50:11 PM
+ * @Createdate May 01, 2014, 04:09:03 PM
  */
 
 if( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 
 $lang_translator['author'] = 'PHAN TAN DUNG (phantandung92@gmail.com)';
-$lang_translator['createdate'] = '26/3/2014, 22:19';
-$lang_translator['copyright'] = '(C) 2014 PHAN TAN DUNG. All rights reserved';
+$lang_translator['createdate'] = 'May 01, 2014, 04:09:03 PM';
+$lang_translator['copyright'] = '@Copyright (C) 2014 PHAN TAN DUNG. All rights reserved';
 $lang_translator['info'] = '';
 $lang_translator['langtype'] = 'lang_module';
 
